604db7115127b66dd770a32a70dcb024fef76695
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-10-01 11:49:40 -0400

    Refactor: removed unnecessary useEffect

--- app/Components/Products.jsx
+++ app/Components/Products.jsx
@@ -1,34 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
 import { allCozyTables } from "../constants";
 import SingleProduct from "./SingleProduct";
 import Wrapper from "../assets/Wrappers/ProductsPage";
 
 const Products = () => {
-  const [activeSection, setActiveSection] = useState([]);
-  let listOfIds = [];
-
   return (
     <>
       {allCozyTables.map((cozyTables, i) => {
         const { title, tables, id } = cozyTables;
-
-        useEffect(() => {
-          const handleScroll = () => {
-            listOfIds.forEach((section) => {
-              const rect = section.getBoundingClientRect();
-              console.log(rect);
-              if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-                setActiveSection(section.id);
-              }
-            });
-          };
-
-          window.addEventListener("scroll", handleScroll);
-          return () => {
-            window.removeEventListener("scroll", handleScroll);
-          };
-        }, []);
 
         return (
           <Wrapper id={id} key={id}>
